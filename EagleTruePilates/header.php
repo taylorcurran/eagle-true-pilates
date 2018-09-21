8edd86317be0e4454d1018c45abedd9217ddf6f2
Taylor Curran <taylorcurran@u.boisestate.edu>
2018-09-20 20:36:39 -0600

    added footer

--- EagleTruePilates/header.php
+++ EagleTruePilates/header.php
@@ -3,11 +3,13 @@
     <meta charset="UTF-8">
     <title>Eagle True Pilates</title>
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300i" rel="stylesheet">
     <link href="css/header.css" rel="stylesheet">
+    <link rel="icon" href="favicon.ico">
 </head>
     <?php require_once "navbar.php"; ?>
-    <div>
-        <h1>Eagle True Pilates</h1>
+    <div id="header">
+        <h1><img id="logo" src="images/eagle.PNG">
+        Eagle True Pilates</h1>
     </div>
-
 </html>
--- EagleTruePilates/navbar.php
+++ EagleTruePilates/navbar.php
@@ -2,7 +2,7 @@
 <body>
 <div class="nav-bar">
     <ul>
-        <li><a <?php if ($thisPage=="home") echo " id=\"currentpage\""; ?> href="home.php">home</a></li>
+        <li><a <?php if ($thisPage=="index") echo " id=\"currentpage\""; ?> href="index.php">home</a></li>
         <li><a <?php if ($thisPage=="services") echo " id=\"currentpage\""; ?> href="services.php">services</a></li>
         <li><a <?php if ($thisPage=="about") echo " id=\"currentpage\""; ?> href="about.php">about</a></li>
         <li><a <?php if ($thisPage=="contact") echo " id=\"currentpage\""; ?> href="contact.php">contact</a></li>
