21ef101de9ee461873a490ccf2189d62eb636281
Taylor Curran <taylorcurran02@gmail.com>
2018-09-24 22:18:22 -0600

    added dropdown

--- EagleTruePilates/about.php
+++ EagleTruePilates/about.php
@@ -6,25 +6,48 @@
     </head>
         <?php require_once "header.php"; ?>
         <div id="about">
-
+            <h2>About</h2>
+            <div>
+                <img src="">
+                <p>This is a paragraph</p>
+            </div>
         </div>
         <h2>Testimonials</h2>
         <div id="testimonials">
-            <div class="box">
-                <h3>"Great Teacher!"</h3>
-                <p>
-                    I was suffering with sciatic pain for a year,
-                    I did not want to take medication or have surgery.
-                    I tried physical therapy and Feldenkrais to relieve the pain.
-                    Both were somewhat helpful but I still could not ride my horse, do chores,
-                    or even walk around the neighborhood without chronic pain.
-                    I was actually looking for a yoga class when I found Julia’s Pilates class.
-                    I began Pilates in May, 2016. I have been going twice weekly and the difference
-                    is amazing. I can ride my horse (number one priority), do yard work, and take walks.
-                    These were all activities that I could not do before Julia’s Pilates classes.
-                    I have my life back! Julia is a great teacher
-                </p>
-                <h5>Ann K.</h5>
+            <div class="column" id="left">
+                <div class="box">
+                    <h3>"Great Teacher!"</h3>
+                    <p>
+                        I was suffering with sciatic pain for a year,
+                        I did not want to take medication or have surgery.
+                        I tried physical therapy and Feldenkrais to relieve the pain.
+                        Both were somewhat helpful but I still could not ride my horse, do chores,
+                        or even walk around the neighborhood without chronic pain.
+                        I was actually looking for a yoga class when I found Julia’s Pilates class.
+                        I began Pilates in May, 2016. I have been going twice weekly and the difference
+                        is amazing. I can ride my horse (number one priority), do yard work, and take walks.
+                        These were all activities that I could not do before Julia’s Pilates classes.
+                        I have my life back! Julia is a great teacher
+                    </p>
+                    <h5>Ann K.</h5>
+                </div>
+            </div>
+            <div class="column" id="right">
+                <div class="box">
+                    <h3></h3>
+                    <p></p>
+                    <h5></h5>
+                </div>
+                <div class="box">
+                    <h3></h3>
+                    <p></p>
+                    <h5></h5>
+                </div>
+                <div class="box">
+                    <h3></h3>
+                    <p></p>
+                    <h5></h5>
+                </div>
             </div>
         </div>
 
--- EagleTruePilates/navbar.php
+++ EagleTruePilates/navbar.php
@@ -3,9 +3,21 @@
 <div class="nav-bar">
     <ul>
         <li><a <?php if ($thisPage=="index") echo " id=\"currentpage\""; ?> href="index.php">home</a></li>
-        <li><a <?php if ($thisPage=="services") echo " id=\"currentpage\""; ?> href="services.php">services</a></li>
+        <li>
+            <div class="dropdown">
+                <button onclick="myFunction()" class="dropbtn" <?php if ($thisPage=="services") echo " id=\"currentpage\""; ?>>services</button>
+                <div id="myDropdown" class="dropdown-content">
+                    <a href="#">Link 1</a>
+                    <a href="schedule.php">schedule</a>
+                </div>
+            </div>
+        </li>
         <li><a <?php if ($thisPage=="about") echo " id=\"currentpage\""; ?> href="about.php">about</a></li>
         <li><a <?php if ($thisPage=="contact") echo " id=\"currentpage\""; ?> href="contact.php">contact</a></li>
     </ul>
 </div>
 </html>
+
+<script>
+
+</script>
