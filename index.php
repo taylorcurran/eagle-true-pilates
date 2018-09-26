cbe854ab15090a0d7cdb3e0c3ea5f3efbf6b3593
Taylor Curran <taylorcurran02@gmail.com>
2018-09-26 17:53:47 -0600

    fixed folder structure

--- index.php
+++ index.php
@@ -1,1 +1,43 @@
-<?php require_once "EagleTruePilates/index.php"; ?>
+<?php $thisPage="index"; ?>
+<html>
+<head>
+    <?php require_once "header.php"; ?>
+    <link href="css/index.css" rel="stylesheet">
+</head>
+<div class="imagewrapper">
+    <div class="image">
+        <img src="images/JoePilatesHome1.jpg">
+    </div>
+    <div class="image">
+        <img src="images/JoePilatesHome2.jpg">
+    </div>
+    <div class="image">
+        <img src="images/JoePilatesHome3.jpg">
+    </div>
+    <div class="image">
+        <img src="images/Joe.jpg">
+    </div>
+    <div class="quote">
+        <p>“Change happens through movement, and movement heals.”</p>
+        <p>- Joeseph Pilates <p>
+    </div>
+</div>
+<div class="boxwrapper">
+    <div class="box" id="about">
+        <h2>About Julia</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit, turpis eget dapibus venenatis, urna mauris tristique libero, nec consectetur ex metus tincidunt justo. Nam sagittis mi et tellus ultrices, id congue tellus semper. Nunc eget neque vitae orci volutpat pulvinar. Aenean lorem dui, elementum ut mi quis, malesuada consequat tellus. In sed ipsum eu augue euismod posuere at gravida leo. Praesent sapien lectus, cursus vitae ante at, laoreet suscipit massa. Sed pretium varius scelerisque. Nam augue elit, feugiat sed venenatis et, commodo nec nibh. Vestibulum nec rutrum sem. </p>
+    </div>
+    <div class="box" id="services">
+        <h2>Services</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit, turpis eget dapibus venenatis, urna mauris tristique libero, nec consectetur ex metus tincidunt justo. Nam sagittis mi et tellus ultrices, id congue tellus semper. Nunc eget neque vitae orci volutpat pulvinar. Aenean lorem dui, elementum ut mi quis, malesuada consequat tellus. In sed ipsum eu augue euismod posuere at gravida leo. Praesent sapien lectus, cursus vitae ante at, laoreet suscipit massa. Sed pretium varius scelerisque. Nam augue elit, feugiat sed venenatis et, commodo nec nibh. Vestibulum nec rutrum sem.
+        </p>
+    </div>
+    <div class="box" id="contact">
+        <h2>Why Pilates?</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit, turpis eget dapibus venenatis, urna mauris tristique libero, nec consectetur ex metus tincidunt justo. Nam sagittis mi et tellus ultrices, id congue tellus semper. Nunc eget neque vitae orci volutpat pulvinar. Aenean lorem dui, elementum ut mi quis, malesuada consequat tellus. In sed ipsum eu augue euismod posuere at gravida leo. Praesent sapien lectus, cursus vitae ante at, laoreet suscipit massa. Sed pretium varius scelerisque. Nam augue elit, feugiat sed venenatis et, commodo nec nibh. Vestibulum nec rutrum sem.
+        </p>
+    </div>
+</div>
+</body>
+<?php require_once "footer.php"; ?>
+</html>
